34f56a4a6c14be7095d3efe5ba23e593faa6b498
CORP\hanlu.feng <hanlu.feng@samsung.com>
2023-01-04 14:51:53 -0800

    add different beacon layout

--- app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
+++ app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
@@ -300,10 +300,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setAlbBeacon(){
+        //https://beaconlayout.wordpress.com/
+        /**
+         *
+        ALTBEACON   "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"
+
+        EDDYSTONE  TLM  "x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15"
+
+        EDDYSTONE  UID  "s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"
+
+        EDDYSTONE  URL  "s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"
+
+        IBEACON  "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+         */
         //Change region then build apk to install on different devices
         val beacon = Beacon.Builder()
             .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
-            .setId2("2")
+            .setId2("3")
             .setId3("2")
             .setManufacturer(0x0118)
             .setTxPower(-59)
